fc3850559d76e5683d38cf215e3b8eaee30f447b
unknown <b160086@nitsikkim.ac.in>
2022-03-26 23:57:06 +0530

    project setup and added Button,ButtonGroup,Icon from material UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, ButtonGroup } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup variant="contained">
+          <Button endIcon={<SaveIcon />} size="large" color="primary">
+            Save
+          </Button>
+          <Button endIcon={<DeleteIcon />} size="large" color="secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
