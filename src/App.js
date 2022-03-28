66284e9ec733309fcf1eb342b78c2b17d5a7b4c8
unknown <b160086@nitsikkim.ac.in>
2022-03-29 00:59:12 +0530

    material UI checkbox example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, ButtonGroup } from "@material-ui/core";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
+import { Checkbox } from "@material-ui/core";
+import { FormControlLabel } from "@material-ui/core";
+
+function CheckBoxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          icon={<DeleteIcon />}
+        />
+      }
+      label="Test Checkbox"
+    />
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckBoxExample />
         <ButtonGroup variant="contained">
           <Button endIcon={<SaveIcon />} size="large" color="primary">
             Save
