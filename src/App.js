f9cd55847ff908464d85f43d2544d33c5eabfc54
unknown <b160086@nitsikkim.ac.in>
2022-03-31 01:08:59 +0530

    material UI Typography and Grid

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { Checkbox } from "@material-ui/core";
 import { FormControlLabel } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, ThemeProvider } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core";
+import { orange } from "@material-ui/core/colors";
+import { Typography, Container, Paper, Grid } from "@material-ui/core";
+import "fontsource-roboto";
 
 const useStyles = makeStyles({
   root: {
@@ -23,6 +27,14 @@ function ButtonStyled() {
   const classes = useStyles();
   return <Button className={classes.root}>Test Styled Button</Button>;
 }
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+  },
+});
 
 function CheckBoxExample() {
   const [checked, setChecked] = React.useState(true);
@@ -42,28 +54,49 @@ function CheckBoxExample() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ButtonStyled />
-        <TextField
-          variant="filled"
-          color="secondary"
-          label="Enter the email"
-          type="email"
-          placeholder="test@test.com"
-        />
-        <CheckBoxExample />
-        <ButtonGroup variant="contained">
-          <Button endIcon={<SaveIcon />} size="large" color="primary">
-            Save
-          </Button>
-          <Button endIcon={<DeleteIcon />} size="large" color="secondary">
-            Discard
-          </Button>
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <div className="App">
+          <header className="App-header">
+            <Typography variant="h1">Welcome to MUI</Typography>
+            <Typography variant="subtitle1">Lear the MUI</Typography>
+            <ButtonStyled />
+            <Grid container spacing={4} justifyContent="space-between">
+              <Grid item>
+                <Paper style={{ width: 50, height: 70 }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{ width: 50, height: 70 }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{ width: 50, height: 70 }} />
+              </Grid>
+              <Grid item>
+                <Paper style={{ width: 50, height: 70 }} />
+              </Grid>
+            </Grid>
+
+            {/* <TextField
+              variant="filled"
+              color="secondary"
+              label="Enter the email"
+              type="email"
+              placeholder="test@test.com"
+            /> */}
+            <CheckBoxExample />
+            <ButtonGroup variant="contained">
+              <Button endIcon={<SaveIcon />} size="large" color="primary">
+                Save
+              </Button>
+              <Button endIcon={<DeleteIcon />} size="large" color="secondary">
+                Discard
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
