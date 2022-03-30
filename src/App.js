643129b50370f95ff095826c61810247856776a7
unknown <b160086@nitsikkim.ac.in>
2022-03-31 00:35:26 +0530

    material UI makeStyles example

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { Checkbox } from "@material-ui/core";
 import { FormControlLabel } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #333, #999)",
+    border: 0,
+    borderRadius: 15,
+    color: "white",
+    padding: "0 30px",
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>;
+}
 
 function CheckBoxExample() {
   const [checked, setChecked] = React.useState(true);
@@ -28,6 +44,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonStyled />
         <TextField
           variant="filled"
           color="secondary"
